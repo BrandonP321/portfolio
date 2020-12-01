ebe53bdbac59f7fc4973d8317acfd9410aa8a6ef
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-01 00:30:33 -0800

    animated skill percentages as counter on page load

--- src/components/SkillBar/index.js
+++ src/components/SkillBar/index.js
@@ -2,13 +2,27 @@ import React, { useState, useEffect } from 'react'
 import './index.css'
 
 export default function SkillBar(props) {
+    const [percent, setPercent] = useState(0)
+
+    // when percent changes (and on load), increment percent for counting animation
+    useEffect(() => {
+        console.log(percent)
+        // if percent has not reached the specified percent, increment
+        if (percent != props.percent) {
+            setTimeout(() => {
+                const newPercent = percent + 1
+                setPercent(newPercent)
+            }, 1000 / props.percent)
+        }
+    }, [percent])
+
     const styles = {
         width: `${props.percent}%`
     }
 
     return (
         <div className='skill'>
-            <p className='skill-text'>{props.skill}<span className='skill-percent'>{props.percent}%</span></p>
+            <p className='skill-text'>{props.skill}<span className='skill-percent'>{percent}%</span></p>
             <div className='skill-bar'>
                 <div className='skill-bar-fill' style={styles}></div>
             </div>

e8a86bf4f91f1e535ef94b565793c09f3af425df
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-02 12:43:16 -0800

    added vertical navbar with appropriate links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,20 +6,32 @@ export default function Header() {
         <header>
             <a href='/' className='white-text header-logo'>Brand<span className='red-text'>o</span>n</a>
             <div className='vertical-nav-bar'>
-                <div className='navbar-spacer'></div>
+                <div className='navbar-spacer gray-bg'></div>
                 <div className='nav-item-container'>
-                    <a href='#'><i class="fas fa-home nav-icon"></i></a>
+                    <a className='nav-item-link' href='#'>
+                        <i class="fas fa-home nav-icon"></i>
+                        <p className='nav-item-text red-text'>Home</p>
+                    </a>
                 </div>
                 <div className='nav-item-container'>
-                    <a href='#about'><i class="fas fa-user nav-icon"></i></a>
+                    <a className='nav-item-link' href='#about'>
+                        <i class="fas fa-user nav-icon"></i>
+                        <p className='nav-item-text red-text'>About</p>
+                    </a>
                 </div>
                 <div className='nav-item-container'>
-                    <a href='#skills'><i class="fas fa-cog nav-icon"></i></a>
+                    <a className='nav-item-link' href='#skills'>
+                        <i class="fas fa-cog nav-icon"></i>
+                        <p className='nav-item-text red-text'>Skills</p>
+                    </a>
                 </div>
                 <div className='nav-item-container'>
-                    <a href='#projects'><i class="fas fa-tasks nav-icon"></i></a>
+                    <a className='nav-item-link' href='#projects'>
+                        <i class="fas fa-tasks nav-icon"></i>
+                        <p className='nav-item-text red-text'>Projects</p>
+                    </a>
                 </div>
-                <div className='navbar-spacer'></div>
+                <div className='navbar-spacer gray-bg'></div>
             </div>
         </header>
     )

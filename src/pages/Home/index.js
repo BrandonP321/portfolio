7050c942e7170b64d2e0deb3b46e365bdbfb57b5
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-01 16:09:02 -0800

    added basic contact form with out functionality

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import SkillBar from '../../components/SkillBar'
+import Footer from '../../components/Footer'
 import Hero from '../../components/Hero'
 import ProjectCard from '../../components/ProjectCard'
 import projects from '../../projects.json'
+import SkillBar from '../../components/SkillBar'
 import './index.css'
+import './contact.css'
 
 export default function Home() {
     return (
@@ -29,6 +31,26 @@ export default function Home() {
                     <div className='portfolios-container'>
                         {projects.map(project => <ProjectCard projectInfo={project} />)}
                     </div>
+                </div>
+                <div className='contact-section'>
+                    <h2 className='section-header white-text'>Contact Me</h2>
+                    <form className='contact-form'>
+                        <div className='contact-fields-flex'>
+                            <div className='input-container name-input-container'>
+                                <input className='contact-input name-input' type='text' placeholder='Name' />
+                            </div>
+                            <div className='input-container email-input-container'>
+                                <input className='contact-input email-input' type='email' placeholder='E-mail' />
+                            </div>
+                            <div className='input-container subject-input-container'>
+                                <input className='contact-input subject-input' type='text' placeholder='Subject' />
+                            </div>
+                            <div className='input-container message-textarea-container'>
+                                <textarea className='contact-input message-textarea' placeholder='Message'></textarea>
+                            </div>
+                        </div>
+                        <button className='send-btn red-bg white-text'>Send</button>
+                    </form>
                 </div>
             </div>
         </>

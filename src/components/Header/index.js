a6d587f187c209f50e4dc3634bcbbebadb121693
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-31 00:46:32 -0800

    added mobile responsiveness to mobile menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,18 @@ import { Router, Link } from 'react-router-dom'
 import './index.css'
 
 export default function Header() {
+    const focusNav = () => {
+        // focus the nav bar
+        document.querySelector('.name-input').focus();
+        console.log('focused')
+    }
+
     return (
         <header>
             <a href='/' className='white-text header-logo'>Brand<span className='red-text'>o</span>n</a>
+            <div className='menu-collapse'>
+                <i class="fas fa-bars menu-icon" onClick={focusNav}></i>
+            </div>
             <div className='horizontal-nav-bar'>
                 <a href='/#' className='white-text nav-link'>Home</a>
                 <a href='/#about' className='white-text nav-link'>About Me</a>
@@ -15,15 +24,15 @@ export default function Header() {
             </div>
             <div className='vertical-nav-bar'>
                 <div className='navbar-spacer gray-bg'></div>
-                    <a href='https://www.linkedin.com/in/brandon-phillips-2554801a1/' target='_blank'>
-                        <i class="fab fa-linkedin-in nav-social-icon"></i>
-                    </a>
-                    <a href='https://github.com/BrandonP321' target='_blank'>
-                        <i class="fab fa-github-alt nav-social-icon"></i>
-                    </a>
-                    <a href='https://stackoverflow.com/users/13790075/brandonp321' target='_blank'>
-                        <i class="fab fa-stack-overflow nav-social-icon"></i>
-                    </a>
+                <a href='https://www.linkedin.com/in/brandon-phillips-2554801a1/' target='_blank'>
+                    <i class="fab fa-linkedin-in nav-social-icon"></i>
+                </a>
+                <a href='https://github.com/BrandonP321' target='_blank'>
+                    <i class="fab fa-github-alt nav-social-icon"></i>
+                </a>
+                <a href='https://stackoverflow.com/users/13790075/brandonp321' target='_blank'>
+                    <i class="fab fa-stack-overflow nav-social-icon"></i>
+                </a>
                 <div className='navbar-spacer gray-bg'></div>
             </div>
             <p className='header-copyright gray-text'>&#169; Brandon 2020</p>

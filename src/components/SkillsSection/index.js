f943f745c8687387ccc47b36e77f0abca3f00e18
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-01 02:36:19 -0800

    added components for several more skill svg icons

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { Carousel } from 'react-bootstrap'
+import IllustratorLogo from '../logos/IllustratorLogo'
+import PhotoshopLogo from '../logos/PhotoshopLogo'
+import JestLogo from '../logos/JestLogo'
+import SocketIOLogo from '../logos/SocketIOLogo'
 import './index.css'
 
 export default function SkillsSection() {
@@ -13,7 +17,7 @@ export default function SkillsSection() {
         <div>
             <h2 className='section-header white-text'>Skills</h2>
             <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
+                <Carousel.Item interval={5000}>
                     <div className='skills-flex'>
                         <div className='skill-container'>
                             <i class="fab fa-html5 skill-icon"></i>
@@ -41,8 +45,16 @@ export default function SkillsSection() {
                         </div>
                     </div>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item interval={5000}>
                     <div className='skills-flex'>
+                        <div className='skill-container'>
+                            <JestLogo  className='skill-icon-svg'/>
+                            <p className='skill-text'>jest</p>
+                        </div>
+                        <div className='skill-container'>
+                            <SocketIOLogo className='skill-icon-svg' />
+                            <p className='skill-text'>Socket.io</p>
+                        </div>
                         <div className='skill-container'>
                             <i class="fab fa-bootstrap skill-icon"></i>
                             <p className='skill-text'>Bootstrap</p>
@@ -52,20 +64,12 @@ export default function SkillsSection() {
                             <p className='skill-text'>GitHub</p>
                         </div>
                         <div className='skill-container'>
-                            <i class="fab fa-js-square skill-icon"></i>
-                            <p className='skill-text'>JavaScript</p>
+                            <IllustratorLogo className='skill-icon-svg' />
+                            <p className='skill-text'>Illustrator</p>
                         </div>
                         <div className='skill-container'>
-                            <i class="fab fa-react skill-icon"></i>
-                            <p className='skill-text'>React</p>
-                        </div>
-                        <div className='skill-container'>
-                            <i class="fas fa-database skill-icon"></i>
-                            <p className='skill-text'>MySQL & NoSQL</p>
-                        </div>
-                        <div className='skill-container'>
-                            <i class="fab fa-node skill-icon"></i>
-                            <p className='skill-text'>Node.js</p>
+                            <PhotoshopLogo className='skill-icon-svg' />
+                            <p className='skill-text'>Photoshop</p>
                         </div>
                     </div>
                 </Carousel.Item>

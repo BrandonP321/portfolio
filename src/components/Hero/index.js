6ccdb70c06ea3a66685d52b14d26bbf1aee0d077
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-31 21:30:54 -0800

    added full mobile responsiveness to hero img/text

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,12 +3,16 @@ import './index.css'
 
 export default function Hero() {
     return (
-        <div>
-            <img className='hero img-fluid' src='./assets/images/stock-hero-edit.png' alt='placeholder hero img' />
-            <div className='hero-text-container'>
-                <p className='hero-my-title white-text'>Full-Stack Web Developer</p>
-                <h1 className='hero-title white-text'>Brand<span className='red-text'>o</span>n</h1>
-                <p className='hero-sub-text gray-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut commodo magna sed sem accumsan, ut cursus justo pretium.</p>
+        <div className='home-hero-wrapper'>
+            <div className='hero-img-wrapper'>
+                <img className='hero-img' src='./assets/images/stock-hero-edit.png' alt='placeholder hero img' />
+            </div>
+            <div className='hero-text-outer-wrapper'>
+                <div className='hero-text-container'>
+                    <p className='hero-my-title white-text'>Full-Stack Web Developer</p>
+                    <h1 className='hero-title white-text'>Brand<span className='red-text'>o</span>n</h1>
+                    <p className='hero-sub-text gray-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut commodo magna sed sem accumsan, ut cursus justo pretium.</p>
+                </div>
             </div>
         </div>
     )

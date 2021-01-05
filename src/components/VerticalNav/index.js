8785d909c197efffebd5c83172a8baa12ce13d8c
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-04 20:00:39 -0800

    added transition to vertical nav leaving screen

--- src/components/VerticalNav/index.js
+++ src/components/VerticalNav/index.js
@@ -7,17 +7,17 @@ export default function VerticalNav() {
             <div className='vertical-nav-flex'>
                 <div className='navbar-spacer gray-bg'></div>
                 <a href='https://www.linkedin.com/in/brandon-phillips-2554801a1/' target='_blank'>
-                    <i class="fab fa-linkedin-in nav-social-icon"></i>
+                    <i className="fab fa-linkedin-in nav-social-icon"></i>
                 </a>
                 <a href='https://github.com/BrandonP321' target='_blank'>
-                    <i class="fab fa-github-alt nav-social-icon"></i>
+                    <i className="fab fa-github-alt nav-social-icon"></i>
                 </a>
                 <a href='https://stackoverflow.com/users/13790075/brandonp321' target='_blank'>
-                    <i class="fab fa-stack-overflow nav-social-icon"></i>
+                    <i className="fab fa-stack-overflow nav-social-icon"></i>
                 </a>
                 <div className='navbar-spacer gray-bg'></div>
             </div>
-            <p className='header-copyright gray-text'>&#169; Brandon 2020</p>
+            <p className='nav-copyright gray-text'>&#169; Brandon 2020</p>
         </div>
     )
 }

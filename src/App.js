cb8fe1fa5c122881378d3a7c71acc7d78182e840
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-15 02:39:58 -0800

    cleaned up files and added meta tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import ProjectOne from './pages/ProjectOne'
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer'
 import VerticalNav from './components/VerticalNav'

40965b140475eec737cd2449d3c00478824f26c7
Brandon Phillips <brandon.phillips976@gmail.com>
2021-02-05 21:44:14 -0800

    converted most images to webp and compressed some

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,7 +8,7 @@ export default function Hero() {
                 <img className='hero-img' src='./assets/images/hero.png' alt='placeholder hero img' />
             </div>
             <div className='hero-text-outer-wrapper'>
-                <div className='hero-text-container' useRef>
+                <div className='hero-text-container'>
                     <p className='hero-my-title white-text'>Full-Stack Web Developer</p>
                     <h1 className='hero-title white-text'>Brand<span className='red-text'>o</span>n</h1>
                     <p className='hero-sub-text gray-text'>Results-oriented web developer with a passion for building impactful web applications with the latest web technologies.</p>
